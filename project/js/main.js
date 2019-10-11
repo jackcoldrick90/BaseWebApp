fa4e994480289c625028717552159dbc79319086
Jack Coldrick <jackcoldrick90@gmail.com>
2019-10-11 09:59:56 +0100

    added search and error handling

--- project/js/main.js
+++ project/js/main.js
@@ -1,10 +1,18 @@
 function getWeather(searchQuery){
   var url = "https://api.openweathermap.org/data/2.5/weather?q="+searchQuery+"&units=metric&APPID="+apiKey;
+  
+  $(".city").text("");
+  $(".temp").text((""));
+  $(".error-message").text((""));
+  
   $.ajax(url, {
     success: function(data){
       console.log(data);
       $(".city").text(data.name);
       $(".temp").text(data.main.temp);
+    },
+    error: function(error){
+      $(".error-message").text("An error occured");
     }
   });
 }
